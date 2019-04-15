819d4ee4c329d41c27bde57fcf632c1c001a3ea3
James Cox <jamesncox@yahoo.com>
2019-04-15 13:06:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,8 +10,8 @@ def key_for_min_value(name_hash)
     if lowest_value == nil || value < lowest_value
       lowest_value = value
       lowest_key = key
-      binding.pry
     end
   end
   lowest_key
+  binding.pry
 end
