5031392c80ab39055c33e8bc23fbaae587252348
James Cox <jamesncox@yahoo.com>
2019-04-15 13:04:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,13 @@
 require 'pry'
 
 def key_for_min_value(name_hash)
-  lowest_key = nil 
-  lowest_value = nil
+  lowest_key = 0 
+  lowest_value = 0
   name_hash.each do |key, value|
-    if lowest_value == nil || value < lowest_value
+    if lowest_value == 0 || value < lowest_value
       lowest_value = value
       lowest_key = key
+    end
   end
   lowest_key
 end
